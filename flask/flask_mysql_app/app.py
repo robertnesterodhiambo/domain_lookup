b53a42d978f81939504a75f527bfbb5c183a1ee3
Robert Nester Odhiambo <Odhiamborobert447@gmail.com>
2025-07-20 19:55:03 +0300

    flask

---
+++ flask/flask_mysql_app/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+import mysql.connector
+from db_config import DB_CONFIG
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    conn = mysql.connector.connect(**DB_CONFIG)
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM subdomain")  # Updated table name
+    records = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return render_template('index.html', records=records)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ flask/flask_mysql_app/db_config.py
@@ -0,0 +1,6 @@
+DB_CONFIG = {
+    'user': 'root',
+    'password': 'root235',
+    'host': '157.90.17.11',
+    'database': 'sys'
+}
